867a11fe03d9e819dde36d8343d5c13130f8a61d
Thiago Emanoel <thiagoemanoel448@gmail.com>
2023-07-09 18:11:51 -0300

    Feat: arrow left on screen

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 import LoginScreen from './src/screens/auth/LoginScreen/LoginScreen';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import SignUpScreen from './src/screens/auth/SignUpScreen/SignUpScreen';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <LoginScreen />
+        <SignUpScreen />
       </ThemeProvider>
     </SafeAreaProvider>
   );
