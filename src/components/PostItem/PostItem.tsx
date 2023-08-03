14aeb2e9f449d5ab7df64e775fe52d708577f300
Thiago Emanoel <thiagoemanoel@Air-de-Thiago.lan>
2023-08-03 08:45:51 -0300

    feat: header home

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -15,7 +15,7 @@ interface Props {
 
 export function PostItem({post}: Props) {
   return (
-    <Box mb="s24">
+    <Box mb="s24" paddingHorizontal="s24">
       <PostHeader author={post.author} />
       <PostImage imageURL={post.imageURL} />
       <PostActions
--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -6,19 +6,25 @@ import {useNavigation} from '@react-navigation/native';
 
 import {useAppSafeArea} from '../../hooks/useAppSafeArea';
 import {useAppTheme} from '../../hooks/useAppTheme';
-import {Box, TouchableOpacityBox} from '../Box/Box';
+import {Box, BoxProps, TouchableOpacityBox} from '../Box/Box';
 import {Icon} from '../Icon/Icon';
 import {Text} from '../Text/Text';
 
 import {ScrollViewContainer, ViewContainer} from './components/ScreenContainer';
 
-interface ScreenProps {
+interface ScreenProps extends BoxProps {
   children: React.ReactNode;
   canGoBack?: boolean;
   scrollable?: boolean;
 }
 
-export function Screen({children, scrollable, canGoBack = false}: ScreenProps) {
+export function Screen({
+  children,
+  scrollable,
+  canGoBack = false,
+  style,
+  ...boxProps
+}: ScreenProps) {
   const {top, bottom} = useAppSafeArea();
   const {colors} = useAppTheme();
   const navigation = useNavigation();
@@ -31,7 +37,8 @@ export function Screen({children, scrollable, canGoBack = false}: ScreenProps) {
       <Container backgroundColor={colors.background}>
         <Box
           paddingHorizontal="s24"
-          style={{paddingTop: top, paddingBottom: bottom}}>
+          style={[{paddingTop: top, paddingBottom: bottom}, style]}
+          {...boxProps}>
           {canGoBack && (
             <TouchableOpacityBox
               mb="s24"
