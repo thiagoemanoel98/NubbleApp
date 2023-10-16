add066a5417754354fef2fc455334a07aab88785
Thiago Emanoel <thiagoemanoel448@gmail.com>
2023-10-16 15:37:32 -0300

    Feat: Loading, Error and Empty List States

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -6,13 +6,26 @@ import {Post, postService} from '@domain';
 import {PostItem, Screen} from '@components';
 import {AppTabScreenProps} from '@routes';
 
+import {HomeEmpty} from './components/HomeEmpty';
 import {HomeHeader} from './components/HomeHeader';
 
 export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
   const [postList, setPostList] = useState<Post[]>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  async function fetchData() {
+    try {
+      setLoading(true);
+      const list = await postService.getList();
+      setPostList(list);
+    } catch (err) {
+      setError(true);
+    }
+  }
 
   useEffect(() => {
-    postService.getList().then(list => setPostList(list));
+    fetchData();
   }, []);
 
   function renderItem({item}: ListRenderItemInfo<Post>) {
@@ -25,7 +38,11 @@ export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
         data={postList}
         keyExtractor={item => item.id}
         renderItem={renderItem}
+        contentContainerStyle={{flex: postList?.length === 0 ? 1 : undefined}}
         ListHeaderComponent={<HomeHeader />}
+        ListEmptyComponent={
+          <HomeEmpty refetch={fetchData} error={error} loading={loading} />
+        }
       />
     </Screen>
   );
@@ -35,4 +52,5 @@ const $screen: StyleProp<ViewStyle> = {
   paddingTop: 0,
   paddingBottom: 0,
   paddingHorizontal: 0,
+  flex: 1,
 };
