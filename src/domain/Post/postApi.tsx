19ee8a20cabe530d6f0ffe0782e66e5b84b87708
Thiago Emanoel <thiagoemanoel448@gmail.com>
2023-10-16 14:07:53 -0300

    Feat: adapater api

--- src/domain/Post/postApi.tsx
+++ src/domain/Post/postApi.tsx
@@ -1,7 +1,20 @@
 import {postListMock} from './postListMock';
-import {Post} from './types';
+import {Post} from './postTypes';
 
 async function getList(): Promise<Post[]> {
+  let headerList = {
+    Authorization:
+      'Bearer MQ.02MN_jo4_ZN57n6FcScHve5a5cy6m2v7DgboEIxdqEWUQgwsdWcVYskytoR9',
+  };
+
+  const response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: headerList,
+  });
+
+  let data = await response.json();
+  console.log('Pego: ', data);
+
   await new Promise(resolve => setTimeout(() => resolve(''), 1000));
   return postListMock;
 }
